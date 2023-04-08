36566108e86f907656f8ed9ab603026f3f3962f9
Josef Hrebec <josefhrebec02@gmail.com>
2023-04-08 23:18:23 +0200

    Add basic fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import randomWords from "random-words";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [word, setWord] = useState("");
+
+  const fetchPhono = () => {
+    let rand: string[] = randomWords(1);
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${rand[0]}`).then(
+      (res) =>
+        res
+          .json()
+          .then((res) => res[0])
+          .then((res) => setWord(res.phonetic))
+    );
+  };
+
+  useEffect(() => {
+    fetchPhono();
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{word}</div>
+      <button onClick={fetchPhono}>Click me</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
